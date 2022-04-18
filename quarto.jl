9cca3c4aa771026fdcf6eef6f86d9df827ccd6b2
Paulo Bruno <pbrunosousa@gmail.com>
2022-04-18 17:30:13 +0200

    Minor changes in render function
    1. Change dot for middle dot.
    2. Blank space before the first character.

--- quarto.jl
+++ quarto.jl
@@ -84,9 +84,9 @@ function render(env::QuartoEnv)
     for j ∈ 1:4
         for i ∈ 1:4
             if (0x0f < env.board[i, j])
-                print(symbollut[(0x0f & env.board[i, j]) + 0x01], ' ')
+                print(' ', symbollut[(0x0f & env.board[i, j]) + 0x01])
             else
-                print(". ")
+                print(" ·")
             end
         end
         println()
